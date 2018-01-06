b26c4ddf60433d37d24b1b359e3ca62989d012b5
Josh Klein <jGlass314@gmail.com>
2018-01-05 19:24:21 -0800

    fixed messed up spacing

--- react-client/src/components/Search.jsx
+++ react-client/src/components/Search.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 
 class Search extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            term: ''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
     }
+  }
 
-    onChange(e) {
-        this.setState({
-            term: e.target.value
-        })
-    }
+  onChange(e) {
+    this.setState({
+      term: e.target.value
+    })
+  }
 
-    onClick() {
-        this.props.callback(this.state.term);
-        this.setState({
-            term: ''
-        });
-    }
+  onClick() {
+    this.props.callback(this.state.term);
+    this.setState({
+      term: ''
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <input type='text' value={this.state.term} style={{'background-color':'#c1f2c7'}} onChange={(event) => this.onChange(event)} />
-                <button type='button' onClick={() => this.onClick()}>Search</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <input type='text' value={this.state.term} style={{'background-color':'#c1f2c7'}} onChange={(event) => this.onChange(event)} />
+        <button type='button' onClick={() => this.onClick()}>Search</button>
+      </div>
+    )
+  }
 }
 
 export default Search;
